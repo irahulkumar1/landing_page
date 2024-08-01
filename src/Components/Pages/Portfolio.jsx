d33ef3bebaeb607ce1566df948befb2ec606ad33
Shantanu Singh <129811851+frontendWithShan@users.noreply.github.com>
2024-08-01 21:47:02 +0530

    Basic structure of Services

---
+++ src/Components/Pages/Portfolio.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Portfolio = () => {
+  return (
+    <div>
+      <h1>Portfolio</h1>
+      <p>This is the Portfolio page.</p>
+    </div>
+  );
+};
+
+export default Portfolio;
